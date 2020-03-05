964acca6635459ff4515d3ac7ae4696a1a1ceba3
thomascarleberg <thomas_carlsson@hotmail.com>
2020-03-05 15:19:13 +0100

    updates

---
+++ GUI-Kassörska/GUI-Kassörska/Order.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GUI_Kassörska
+{
+    public class Order
+    {
+        private int orderNumber;
+
+        public Order()
+        {
+
+        }
+
+        public int OrderNumber
+        {
+            get { return orderNumber; }
+            set { }
+        }
+    }
+}
