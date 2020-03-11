f3ad744fa7be0be2aa1b4a61964f05043b8ee8ab
Harliin <56713048+Harliin@users.noreply.github.com>
2020-03-11 14:58:04 +0100

    Lade till router och reactive ui för main window

--- GUI-Beställning/GUI-Beställning/ViewModels/MainWindowViewModel.cs
+++ GUI-Beställning/GUI-Beställning/ViewModels/MainWindowViewModel.cs
@@ -1,17 +1,57 @@
 ﻿using GUI_Beställning.ViewModels.Commands;
+using GUI_Beställning.Views;
+using ReactiveUI;
+using Splat;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Windows.Input;
 
 namespace GUI_Beställning.ViewModels
 {
-    public class MainWindowViewModel : INotifyPropertyChanged
+    public class MainWindowViewModel : ReactiveObject, IScreen
     {
-        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+        public RoutingState Router { get; }
 
-        
+        #region Commands
+        public ReactiveCommand<Unit, IRoutableViewModel> PizzaMenu { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel> PastaMenu { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel> SalladMenu { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel> DrinkMenu { get; }
+        public ReactiveCommand<Unit, IRoutableViewModel> ExtraMenu { get; }
+
+        #endregion
+
+
+        public MainWindowViewModel()
+        {
+            Router = new RoutingState();
+
+            Locator.CurrentMutable.Register(() => new PizzaMenuView(), typeof(IViewFor<PizzaMenuViewModel>));
+
+            Locator.CurrentMutable.Register(() => new PastaMenuView(), typeof(IViewFor<PastaMenuViewModel>));
+
+            Locator.CurrentMutable.Register(() => new SalladMenuView(), typeof(IViewFor<SalladMenuViewModel>));
+
+            Locator.CurrentMutable.Register(() => new DrinkMenuView(), typeof(IViewFor<DrinkMenuViewModel>));
+
+            Locator.CurrentMutable.Register(() => new ExtraMenuView(), typeof(IViewFor<ExtraMenuViewModel>));
+
+
+
+            PizzaMenu = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new PizzaMenuViewModel()));
+
+            //PastaMenu = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new PastaMenuViewModel()));
+
+            //SalladMenu = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new SalladMenuViewModel()));
+
+            //DrinkMenu = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new DrinkMenuViewModel()));
+
+            //ExtraMenu = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new ExtraMenuViewModel()));
+
+        }
     }
 }
--- GUI-Beställning/GUI-Beställning/Views/MainWindowView.xaml.cs
+++ GUI-Beställning/GUI-Beställning/Views/MainWindowView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿
 using GUI_Beställning.ViewModels;
+using ReactiveUI;
 using System.Windows;
 
 
@@ -8,12 +9,37 @@ namespace GUI_Beställning.Views
     /// <summary>
     /// Interaction logic for MainWindowView.xaml
     /// </summary>
-    public partial class MainWindowView : Window
+    public partial class MainWindowView : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindowView()
         {
-            this.DataContext = new MainWindowViewModel();
             InitializeComponent();
+
+            this.DataContext = new MainWindowViewModel();
+
+            ViewModel = new MainWindowViewModel();
+
+            this.WhenActivated(disposables =>
+            {
+                this.OneWayBind(ViewModel, x => x.Router, x => x.RoutedViewHost.Router)
+                .DisposeWith(disposables);
+
+                this.BindCommand(ViewModel, x => x.PizzaMenu, x => x.GoPizzaMenu)//PizzaButton
+                    .DisposeWith(disposables);
+
+                this.BindCommand(ViewModel, x => x.PastaMenu, x => x.GoPastaMenu)//PastaButton
+                    .DisposeWith(disposables);
+
+                this.BindCommand(ViewModel, x => x.SalladMenu, x => x.GoSalladMenu)//SalladButton
+                   .DisposeWith(disposables);
+
+                this.BindCommand(ViewModel, x => x.DrinkMenu, x => x.GoDrinkMenu)//DrinkButton
+                   .DisposeWith(disposables);
+
+                this.BindCommand(ViewModel, x => x.ExtraMenu, x => x.GoExtraMenu)//ExtraButton
+                   .DisposeWith(disposables);
+            });
+
         }
     }
 }
