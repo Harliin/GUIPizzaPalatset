32f25ab953cdd42ae54576bd166c8f70cdaf34e9
Pontuspe <pontus-pettersson@hotmail.com>
2020-03-11 12:54:21 +0100

    mer

---
+++ GUI-Beställning/GUI-Beställning/ViewModels/ExtraMenuViewModel.cs
@@ -0,0 +1,37 @@
+﻿using GUI_Beställning.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+
+namespace GUI_Beställning.ViewModels
+{
+    class ExtraMenuViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        public OrderRepository repo = new OrderRepository();
+        public ObservableCollection<Extra> Extras { get; set; }
+
+        public ExtraMenuViewModel()
+        {
+            Extras = new ObservableCollection<Extra>();
+            GetExtras();
+        }
+
+        private void GetExtras()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                Extra extra = new Extra
+                {
+                    OrderID = i + 1,
+                    Name = "Vitlöksbröd",
+                    Price = 20
+                };
+                Extras.Add(extra);
+            }
+        }
+    }
+}
--- GUI-Beställning/GUI-Beställning/Views/ExtraMenuView.xaml.cs
+++ GUI-Beställning/GUI-Beställning/Views/ExtraMenuView.xaml.cs
@@ -1,15 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
+﻿using GUI_Beställning.ViewModels;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
 
 namespace GUI_Beställning.Views
 {
@@ -21,6 +12,7 @@ namespace GUI_Beställning.Views
         public ExtraMenuView()
         {
             InitializeComponent();
+            this.DataContext = new ExtraMenuViewModel();
         }
     }
 }
