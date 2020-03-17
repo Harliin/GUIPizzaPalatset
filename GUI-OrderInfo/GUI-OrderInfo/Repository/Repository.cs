1c38d562706bdacef9efc6c1986499cbc3a98fbf
Hitman08 <56691277+Hitman08@users.noreply.github.com>
2020-03-17 09:56:34 +0100

    repository added

---
+++ GUI-OrderInfo/GUI-OrderInfo/Repository/Repository.cs
@@ -0,0 +1,36 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace GUI_OrderInfo.Repository
+{
+    class Repository
+    {
+        private string ConnectionString { get; }
+        private SqlConnection connection { get; }
+        public static int Backend { get; set; }
+        public Repository()
+        {
+            ConnectionString = "Data Source=SQL6009.site4now.net;Initial Catalog=DB_A53DDD_Grupp1;User Id=DB_A53DDD_Grupp1_admin;Password=Password123;";
+            connection = new SqlConnection(ConnectionString);
+            connection.Open();
+        }
+        private IDbConnection Connection
+        {
+            get
+            {
+                IDbConnection con;
+                con = new SqlConnection(ConnectionString);
+                con.Open();
+                return con;
+            }
+        }
+        // Beställnings Repositorys
+        //public async Task RemovePizzaFromOrder(int orderID, int pizzaID)
+        //{
+        //    using (IDbConnection con = Connection)
+        //    {
+        //        await connection.QueryAsync<Pizza>("\"RemovePizzaFromOrder\"", new { orderid = orderID, pizzaid = pizzaID }, commandType: CommandType.StoredProcedure);
+        //    }
+        //}
+    }
+}
