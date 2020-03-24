746967cbb175800a43f9eff3213bafe57b237056
thomascarleberg <thomas_carlsson@hotmail.com>
2020-03-24 11:02:11 +0100

    ändringar

--- GUI-Kassörska/GUI-Kassörska/MainWindow.xaml.cs
+++ GUI-Kassörska/GUI-Kassörska/MainWindow.xaml.cs
@@ -1,18 +1,5 @@
 ﻿using GUI_Kassörska.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace GUI_Kassörska
 {
@@ -30,7 +17,5 @@ namespace GUI_Kassörska
             InitializeComponent();
             this.DataContext = Main;
         }
-
-        
     }
 }
--- GUI-Kassörska/GUI-Kassörska/ViewModels/Order.cs
+++ GUI-Kassörska/GUI-Kassörska/ViewModels/Order.cs
@@ -1,16 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Text;
 
 namespace GUI_Kassörska.ViewModels
 {
-    public class Order // : ObservableObject
+    public class Order : ObservableCollection<Order>
     {
         public int ID { get; set; }
         public eStatus Status { get; set; }
         public int EmployeeID { get; set; }
         public enum eStatus { UnderBeställning = 1, Tillagning = 2, Klar = 3, Avhämtad = 4 }
-        public List<Order> Orders { get; set; }
+        public ObservableCollection<Order> Orders { get; set; }
     }
 }
