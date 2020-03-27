1dd0c8b1b9ca2b637d8aa1c1642646b27fdcf546
Pontuspe <pontus-pettersson@hotmail.com>
2020-03-27 11:55:21 +0100

    Update PaymentViewModel.cs

--- GUI-Beställning/GUI-Beställning/ViewModels/PaymentViewModel.cs
+++ GUI-Beställning/GUI-Beställning/ViewModels/PaymentViewModel.cs
@@ -21,10 +21,10 @@ namespace GUI_Beställning.ViewModels
         #endregion
         public Order CurrentOrder { get; set; }
         public OrderRepository repo = new OrderRepository();
-        public List<string> CurrentOrderName { get; set; }
-        public List<int> CurrentOrderPrice { get; set; }
-        public List<int> CurrentFoodID { get; set; }
-        public List<Order.eFoodType> foodType { get; set; }
+        public ObservableCollection<string> CurrentOrderName { get; set; }
+        public ObservableCollection<int> CurrentOrderPrice { get; set; }
+        public ObservableCollection<int> CurrentFoodID { get; set; }
+        public ObservableCollection<string> FoodType { get; set; }
 
         public RelayCommand RemoveCommand { get; set; }
         public ObservableCollection<Order> Orders { get; set; }
@@ -40,20 +40,20 @@ namespace GUI_Beställning.ViewModels
 
         public void ShowOrder()
         {
-            CurrentOrderName = new List<string>();
-            CurrentOrderPrice = new List<int>();
-            CurrentFoodID = new List<int>();
-            foodType = new List<Order.eFoodType>();
+            CurrentOrderName = new ObservableCollection<string>();
+            CurrentOrderPrice = new ObservableCollection<int>();
+            CurrentFoodID = new ObservableCollection<int>();
+            FoodType = new ObservableCollection<string>();
 
 
             var ordersIE = repo.ShowOrderByID(this.OrderID);
             var temp = ordersIE.ToList();
             CurrentOrder = temp[0];
-            CurrentOrder.pizza.ForEach(pizza => { CurrentOrderName.Add(pizza.Name); CurrentOrderPrice.Add(pizza.Price); CurrentFoodID.Add(pizza.ID); foodType.Add(Order.eFoodType.pizza); });
-            CurrentOrder.pasta.ForEach(pasta => { CurrentOrderName.Add(pasta.Name); CurrentOrderPrice.Add(pasta.Price); CurrentFoodID.Add(pasta.ID); foodType.Add(Order.eFoodType.pasta); });
-            CurrentOrder.sallad.ForEach(sallad => { CurrentOrderName.Add(sallad.Name); CurrentOrderPrice.Add(sallad.Price); CurrentFoodID.Add(sallad.ID); foodType.Add(Order.eFoodType.sallad); });
-            CurrentOrder.drink.ForEach(drink => { CurrentOrderName.Add(drink.Name); CurrentOrderPrice.Add(drink.Price); CurrentFoodID.Add(drink.ID); foodType.Add(Order.eFoodType.drink); });
-            CurrentOrder.extra.ForEach(extra => { CurrentOrderName.Add(extra.Name); CurrentOrderPrice.Add(extra.Price); CurrentFoodID.Add(extra.ID); foodType.Add(Order.eFoodType.extra); });
+            CurrentOrder.pizza.ForEach(pizza => { CurrentOrderName.Add(pizza.Name); CurrentOrderPrice.Add(pizza.Price); CurrentFoodID.Add(pizza.ID); FoodType.Add("pizza"); });
+            CurrentOrder.pasta.ForEach(pasta => { CurrentOrderName.Add(pasta.Name); CurrentOrderPrice.Add(pasta.Price); CurrentFoodID.Add(pasta.ID); FoodType.Add("pasta"); });
+            CurrentOrder.sallad.ForEach(sallad => { CurrentOrderName.Add(sallad.Name); CurrentOrderPrice.Add(sallad.Price); CurrentFoodID.Add(sallad.ID); FoodType.Add("sallad"); });
+            CurrentOrder.drink.ForEach(drink => { CurrentOrderName.Add(drink.Name); CurrentOrderPrice.Add(drink.Price); CurrentFoodID.Add(drink.ID); FoodType.Add("drink"); });
+            CurrentOrder.extra.ForEach(extra => { CurrentOrderName.Add(extra.Name); CurrentOrderPrice.Add(extra.Price); CurrentFoodID.Add(extra.ID); FoodType.Add("extra"); });
 
             
         }
@@ -61,7 +61,7 @@ namespace GUI_Beställning.ViewModels
         {
             var values = (object[])parameter;
             var id = (int)values[0];
-            var foodType = (Order.eFoodType)values[1];
+            var foodType = (string)values[1];
         }
     }
 }
