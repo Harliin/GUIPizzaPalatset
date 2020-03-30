76197ae031c625bd9e148c515854e3e518294d93
Harliin <56713048+Harliin@users.noreply.github.com>
2020-03-30 16:10:06 +0200

    la till welcomeview

---
+++ GUI-Beställning/GUI-Beställning/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GUI_Beställning.ViewModels
+{
+    public class WelcomeViewModel : ReactiveObject, IRoutableViewModel
+    {
+        #region For Reactive UI
+        public string UrlPathSegment => "WelcomeMenu";
+
+        public IScreen HostScreen { get; }
+
+        #endregion
+
+        public WelcomeViewModel(IScreen screen = null)
+        {
+            HostScreen = screen ?? Locator.Current.GetService<IScreen>();
+        }
+    }
+}
--- GUI-Beställning/GUI-Beställning/Views/WelcomeView.xaml.cs
+++ GUI-Beställning/GUI-Beställning/Views/WelcomeView.xaml.cs
@@ -1,17 +1,29 @@
-﻿
+﻿using GUI_Beställning.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
 using System.Windows.Controls;
-
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
 namespace GUI_Beställning.Views
 {
     /// <summary>
     /// Interaction logic for WelcomeView.xaml
     /// </summary>
-    public partial class WelcomeView : Page
+    public partial class WelcomeView : ReactiveUserControl<WelcomeViewModel>
     {
         public WelcomeView()
         {
             InitializeComponent();
+            this.DataContext = new WelcomeViewModel();
         }
     }
 }
