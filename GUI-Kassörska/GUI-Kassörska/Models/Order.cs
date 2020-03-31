2ffd6e603bccb0bc5d2236297719a40f7a6a2baa
thomascarleberg <thomas_carlsson@hotmail.com>
2020-03-31 11:16:13 +0200

    Update Order.cs

--- GUI-Kassörska/GUI-Kassörska/Models/Order.cs
+++ GUI-Kassörska/GUI-Kassörska/Models/Order.cs
@@ -13,23 +13,26 @@ namespace GUI_Kassörska.ViewModels
         public eStatus Status { get; set; }
         public int EmployeeID { get; set; }
         public enum eStatus { UnderBeställning = 1, Tillagning = 2, Klar = 3, Avhämtad = 4 }
-        public ObservableCollection<Order> OrderList { get; private set; }
-        public List<Order> Orders { get; set; }
 
+        private ObservableCollection<Order> orders { get; set; }
+        //Observable collection of orders
+        public ObservableCollection<Order> Orders { get { return orders; } private set { } }
+
+        //Create an instance of Cashier repository
         CashierRepository repo = new CashierRepository();
 
         public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
 
         public Order()
         {
-            OrderList = new ObservableCollection<Order>();
+            var Order = new ObservableCollection<Order>();
             
         }
 
-        //public void Populate()
-        //{
-        //    IEnumerable<Order> orders = repo.ShowAllOrders();
-        //    OrderList.AddRange(orders);
-        //}
+        public void Populate()
+        {
+            IEnumerable<Order> orders = repo.ShowAllOrders();
+            Orders.AddRange(orders);
+        }
     }
 }
