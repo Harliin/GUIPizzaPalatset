100e8f5085e23e24865bc0060e4a11962a1001ed
Pontuspe <pontus-pettersson@hotmail.com>
2020-03-31 09:33:30 +0200

    receiptview

---
+++ GUI-Beställning/GUI-Beställning/ViewModels/ReceiptViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ReactiveUI;
+using Splat;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GUI_Beställning.ViewModels
+{
+    class ReceiptViewModel : ReactiveObject, IRoutableViewModel
+    {
+        #region For Reactive UI
+        public string UrlPathSegment => "ReceiptView";
+
+        public IScreen HostScreen { get; }
+
+        #endregion
+
+        public ReceiptViewModel(IScreen screen = null)
+        {
+            HostScreen = screen ?? Locator.Current.GetService<IScreen>();
+        }
+    }
+}
