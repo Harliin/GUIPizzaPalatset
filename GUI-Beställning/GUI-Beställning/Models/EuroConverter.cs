7d6f96a19637a83cc728ebae307cd233d2b2f41e
Pontuspe <pontus-pettersson@hotmail.com>
2020-04-01 14:39:35 +0200

    EuroConverter

--- GUI-Beställning/GUI-Beställning/Models/EuroConverter.cs
+++ GUI-Beställning/GUI-Beställning/Models/EuroConverter.cs
@@ -4,28 +4,36 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GUI_Beställning.Models
 {
     public class EuroConverter
     {
-        public float EuroRate()
-        {
-            ExchangeRates fileNames = new ExchangeRates();
-            using (HttpClient httpClient = new HttpClient())
-            {
-                //httpClient.DefaultRequestHeaders.Add(HeaderNames.Authorization, "Bearer " + GetJWT());
-                string uri = "https://api.exchangeratesapi.io/latest?symbols=USD";
+        //private ExchangeRates _order;
+        //public ExchangeRates Order
+        //{
+        //    get { return _order; }
+        //    set { this.RaiseAndSetIfChanged(ref _order, value); }
 
-                //HttpResponseMessage response = await httpClient.GetAsync(uri);
-                if (response.IsSuccessStatusCode)
-                {
-                    StreamReader reader = new StreamReader(await response.Content.ReadAsStreamAsync(), Encoding.UTF8);
-                    string dataFromWebapi = reader.ReadToEnd();
-                    fileNames = System.Text.Json.JsonSerializer.Deserialize<ExchangeRates>(dataFromWebapi);
-                }
-            }
-            Order = fileNames;
-        }
+        //}
+        //public async Task EuroRateAsync()
+        //{
+        //    ExchangeRates fileNames = new ExchangeRates();
+        //    using (HttpClient httpClient = new HttpClient())
+        //    {
+        //        //httpClient.DefaultRequestHeaders.Add(HeaderNames.Authorization, "Bearer " + GetJWT());
+        //        string uri = "https://api.exchangeratesapi.io/latest?symbols=USD";
+
+        //        HttpResponseMessage response = await httpClient.GetAsync(uri);
+        //        if (response.IsSuccessStatusCode)
+        //        {
+        //            StreamReader reader = new StreamReader(await response.Content.ReadAsStreamAsync(), Encoding.UTF8);
+        //            string dataFromWebapi = reader.ReadToEnd();
+        //            fileNames = System.Text.Json.JsonSerializer.Deserialize<ExchangeRates>(dataFromWebapi);
+        //        }
+        //    }
+            
+        //}
     }
 }
--- GUI-Beställning/GUI-Beställning/ViewModels/ReceiptViewModel.cs
+++ GUI-Beställning/GUI-Beställning/ViewModels/ReceiptViewModel.cs
@@ -1,9 +1,15 @@
-﻿using ReactiveUI;
+﻿using GUI_Beställning.Models.Data;
+using ReactiveUI;
 using Splat;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Reactive;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace GUI_Beställning.ViewModels
 {
@@ -20,11 +26,13 @@ namespace GUI_Beställning.ViewModels
         public static MainWindowViewModel MainWindowViewModel;
         public ObservableCollection<object> RecieptFoods => MainWindowViewModel.Order;
         public int OrderID => MainWindowViewModel.OrderID;
-        public int SEKPrice => MainWindowViewModel.TotalPrice;
+        public float SEKPrice => MainWindowViewModel.TotalPrice;
 
-        public int EuroPrice { get; set; }
+        public float EuroPrice { get; set; }
 
         #endregion
+
+        
         public ReceiptViewModel(MainWindowViewModel viewModel = null,IScreen screen = null)
         {
             HostScreen = screen ?? Locator.Current.GetService<IScreen>();
@@ -33,6 +41,26 @@ namespace GUI_Beställning.ViewModels
             {
                 MainWindowViewModel = viewModel;
             }
+            EuroRateAsync();
+        }
+   
+        public void EuroRateAsync()
+        {
+           
+            ExchangeRates rates = new ExchangeRates();
+            using (WebClient webClient = new WebClient())
+            {
+                string uri = "https://api.exchangeratesapi.io/latest?symbols=SEK";
+                webClient.BaseAddress = uri;
+                var json = webClient.DownloadString(uri);
+                rates = System.Text.Json.JsonSerializer.Deserialize<ExchangeRates>(json);
+            }
+
+            if(rates.Rates.TryGetValue("SEK", out float rate))
+            {
+                EuroPrice = (SEKPrice / rate);   
+            }
+           // return EuroPrice;
         }
     }
 }
