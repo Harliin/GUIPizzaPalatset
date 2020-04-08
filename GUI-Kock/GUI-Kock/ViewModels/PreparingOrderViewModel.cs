43c7f037c1a7fd1f949717344c896fb602972034
AataMie <57095052+AataMie@users.noreply.github.com>
2020-04-08 12:38:13 +0200

    update

--- GUI-Kock/GUI-Kock/ViewModels/PreparingOrderViewModel.cs
+++ GUI-Kock/GUI-Kock/ViewModels/PreparingOrderViewModel.cs
@@ -33,8 +33,6 @@ namespace GUI_Kock.ViewModels
 
         public static ChefRepository repos => LoginViewModel.repo;
 
-        public List<string> PizzaItems { get; private set; }
-
         public Order CurrentOrder { get; private set; }
 
         public ObservableCollection<object> orderItems { get; }
@@ -72,8 +70,7 @@ namespace GUI_Kock.ViewModels
             GoToOrderView = ReactiveCommand.CreateFromObservable(() => Router.Navigate.Execute(new OrderViewModel()));
             UpdateOrder = new RelayCommand(UpdateOrderStatus);
             Timer = new RelayCommand(TimerOn);
-            orderItems = new ObservableCollection<object>();
-            PizzaItems = new List<string>();
+            orderItems = new ObservableCollection<object>(ShowOrder());
         }
 
         private void UpdateOrderStatus(object parameter)
@@ -89,7 +86,21 @@ namespace GUI_Kock.ViewModels
         }
 
 
+        public List<object> ShowOrder()
+        {
+            List<object> OrderList = new List<object>();
+            var ordersIE = repos.ShowOrderByID(CurrentOrder.ID);
+            var temp = ordersIE.ToList();
 
+            CurrentOrder = temp[0];
+            CurrentOrder.pizza.ForEach(pizza => { OrderList.Add(pizza); });
+            CurrentOrder.pasta.ForEach(pasta => { OrderList.Add(pasta); });
+            CurrentOrder.sallad.ForEach(sallad => { OrderList.Add(sallad); });
+            CurrentOrder.drink.ForEach(drink => { OrderList.Add(drink); });
+            CurrentOrder.extra.ForEach(extra => { OrderList.Add(extra); });
+            return OrderList;
+
+        }
 
     }
 }
